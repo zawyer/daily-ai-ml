7a62d8ae2cd334ac244085fe9c60444fab04541c
Zawyer Rashid <229319938+zawyer@users.noreply.github.com>
2026-09-09 20:00:59 +0500

    Add regression line plotting to linear regression
    Refactor code for readability and add plotting functionality.

--- machine-learning/linear_regression.py
+++ machine-learning/linear_regression.py
@@ -1,18 +1,19 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-# Training data
-X_train = np.array([1, 2, 3, 4])
-y_train = np.array([2, 4, 6, 8])
+X_train=np.array([1,2,3,4])
+y_train=np.array([2,4,6,8])
 
-# Test data
-X_test = np.array([5])
-y_test = np.array([10])
+X_test=np.array([5])
+y_test=np.array([10])
 
-# Learn from training data
-m = np.sum((X_train - X_train.mean()) * (y_train - y_train.mean())) / np.sum((X_train - X_train.mean()) ** 2)
+m = np.sum((X_train - X_train.mean()) * (y_train - y_train.mean())) / np.sum(
+    (X_train - X_train.mean()) ** 2
+)
+
 b = y_train.mean() - m * X_train.mean()
 
-# Make prediction on test data
+# Make prediction
 y_pred = m * X_test + b
 
 # Calculate MSE
@@ -23,3 +24,23 @@ print("Intercept:", b)
 print("Actual value:", y_test[0])
 print("Predicted value:", y_pred[0])
 print("MSE:", mse)
+
+# Create regression line
+X_line = np.linspace(1, 5, 100)
+y_line = m * X_line + b
+
+# Plot training data
+plt.scatter(X_train, y_train, label="Training data")
+
+# Plot test data
+plt.scatter(X_test, y_test, label="Test data")
+
+# Plot regression line
+plt.plot(X_line, y_line, label="Regression line")
+
+plt.xlabel("X")
+plt.ylabel("y")
+plt.title("Linear Regression")
+plt.legend()
+plt.show()
+
